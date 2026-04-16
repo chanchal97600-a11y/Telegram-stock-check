5b41a7ab64a916007fb785fc16e0cf23fa009acd
chanchal97600-a11y <chanchal97600@gmail.com>
2026-04-16 22:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -249,12 +249,7 @@ def webhook():
     elif down:
         message = f"📊 {down['stock']}" + format_fundamental(fundamental)
     else:
-        message = " Hello!! Enter a valid NSE stock to get:
-📊 Backtest Performance  
-📈 Buy & Target Levels  
-💡 Market Analysis  
-
-Example: RELIANCE, TCS, INFY "
+        message = " Hello!! Enter a valid NSE stock to get:📊 Backtest Performance  📈 Buy & Target Levels 💡 Market Analysis  Example: RELIANCE, TCS, INFY "
 
     send_message(chat_id, message)
 
