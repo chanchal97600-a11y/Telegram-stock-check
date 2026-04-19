70865cc2317143b2c42438c1ed68990140128999
chanchal97600-a11y <chanchal97600@gmail.com>
2026-04-19 17:15:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,8 +256,8 @@ def create_bar_chart(stock, up_wr, down_wr):
     x = np.array([0, 0.8])
 
     fig, ax = plt.subplots(figsize=(2.1, 3.8), dpi=400)
-    fig.patch.set_facecolor("#aeb5bf")
-    ax.set_facecolor("#aeb5bf")
+    fig.patch.set_facecolor("#aeb5bf")  #BACKGROUNDCOLOR
+    ax.set_facecolor("#aeb5bf")   #BACKGROUNDCOLOR
 
     colors = ["#00bbff", "#007d96"]
     bars = ax.bar(x, values, width=0.45, color=colors, edgecolor="none")
