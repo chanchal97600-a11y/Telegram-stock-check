e24d500c651c824c0f2e51e489d427421963af31
chanchal97600-a11y <chanchal97600@gmail.com>
2026-04-20 14:58:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -292,9 +292,9 @@ def create_bar_chart(stock, up_wr, down_wr):
     for yi, val in zip(y, values):
         bar = FancyBboxPatch(
             (0, yi - bar_height / 2),
-            val/3,
+            val,
             bar_height,
-            boxstyle="round,pad=0,rounding_size=0.25",
+            boxstyle="round,pad=0,rounding_size=02",
             linewidth=0,
             facecolor="none"
         )
