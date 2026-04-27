cefd7e7a66c5b37053e2f99d43e9490a1cda5058
chanchal97600-a11y <chanchal97600@gmail.com>
2026-04-28 03:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -630,17 +630,71 @@ def webhook():
             return "ok"
 
         text = text.strip()
+        # =========================
+        # TRADE COMMANDS
+        # =========================
+
+        if text.lower() == "/trade amount":
+            amt = get_last_amount()
+            send_message(chat_id, f"💰 You can invest ₹{amt:,.2f}")
+            return "ok"
 
 
-# =========================
-# TRADE COMMANDS
-# =========================
+        if text.lower().startswith("/trade buy"):
+            try:
+                p = text.split()
 
-         if text.lower() == "/trade amount":
-             amt = get_last_amount()
+                stock = p[2]
+                buy_date = p[3]
 
-             send_message(chat_id, f"💰 You can invest ₹{amt:,.2f}")
-             return "ok"
+                amt = trade_buy(stock, buy_date)
+
+                send_message(
+                    chat_id,
+                    f"✅ Buy saved\n\n"
+                    f"Stock: {stock.upper()}\n"
+                    f"Date: {buy_date}\n"
+                    f"Locked Amount: ₹{amt:,.2f}"
+                )
+
+            except:
+                send_message(chat_id, "Use:\n/trade buy TCS 24-04-2026")
+
+            return "ok"
+
+
+        if text.lower().startswith("/trade sell"):
+            try:
+                p = text.split()
+
+                result = p[2]
+                stock = p[3]
+                sell_date = p[4]
+
+                initial, final, hold = trade_sell(result, stock, sell_date)
+
+                if initial is None:
+                    send_message(chat_id, "❌ Open trade not found")
+                    return "ok"
+
+                send_message(
+                    chat_id,
+                    f"✅ Sell saved\n\n"
+                    f"Stock: {stock.upper()}\n"
+                    f"Result: {result.upper()}\n"
+                    f"Previous: ₹{initial:,.2f}\n"
+                    f"New Amount: ₹{final:,.2f}\n"
+                    f"Hold Days: {hold}\n\n"
+                    f"💰 Now you can invest ₹{final:,.2f}"
+                )
+
+            except:
+                send_message(chat_id, "Use:\n/trade sell win TCS 25-04-2026")
+
+            return "ok"
+
+ 
+
 
 
          if text.lower().startswith("/trade buy"):
